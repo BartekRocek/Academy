f0951a9716cda18a971ddea501d0a11581ebe654
Rocek <b.roclawski@gmail.com>
2021-03-12 11:08:53 +0100

    Almost done but still needs some improvement to store the array items entered in the previous steps.

--- src/Tickets.java
+++ src/Tickets.java
@@ -1,144 +1,103 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.lang.Integer;
 
 public class Tickets {
 
-    //A method to print out the menu 24/01/2021
-    public static void displayMenuText(int menuItem, int counter, int numberOfRows, int numberOfSeatsInRow,
-                                char[][] cinemaRoom, int rowNumber, int seatNumber, int checkSum,
-                                int rowNumberBooked, int seatNumberBooked) {
+    public static int getNumberOfRows() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("\n" + "1. Show the seats" + "\n" + "2. Buy a ticket" + "\n" + "0. Exit");
-        System.out.print("> ");
-        menuItem = scanner.nextInt();
-        selectMenuItem(menuItem,
-                counter,
-                numberOfRows,
-                numberOfSeatsInRow,
-                cinemaRoom,
-                rowNumber,
-                seatNumber,
-                checkSum,
-                rowNumberBooked,
-                seatNumberBooked);
+        int numberOfRows;
+        numberOfRows = scanner.nextInt();
+
+        while (numberOfRows > 9) {
+            numberOfRows = scanner.nextInt();
+        }
+        return numberOfRows;
     }
 
-    //A method to select a menu item 24/01/2021
-    public static void selectMenuItem(int menuItem, int counter, int numberOfRows, int numberOfSeatsInRow,
-                                     char[][] cinemaRoom, int rowNumber, int seatNumber,int checkSum,
-                                      int rowNumberBooked, int seatNumberBooked) {
-            if (menuItem == 1) {
-                showTheSeats(numberOfRows,
-                numberOfSeatsInRow,
-                cinemaRoom,
-                rowNumber,
-                seatNumber,
-                menuItem,
-                checkSum,
-                rowNumberBooked,
-                seatNumberBooked);
-            }
-            else if (menuItem == 2) {
-                buyTicket(numberOfRows,
-                        numberOfSeatsInRow,
-                        menuItem,
-                        counter,
-                        cinemaRoom);
-            } else if (menuItem == 0) {
-                System.exit(0);
-            }
-        }
-
-    //A method for buying a ticket 24/01/2021*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-    public static void buyTicket(int numberOfRows, int numberOfSeatsInRow, int menuItem, int counter, char[][] cinemaRoom) {
-        int checkSum = 0;
-        ++checkSum;
+    public static int getNumberOfSeatsInRow() {
         Scanner scanner = new Scanner(System.in);
+        int numberOfSeatsInRow;
+        numberOfSeatsInRow = scanner.nextInt();
+        return numberOfSeatsInRow;
+    }
 
-        System.out.println("\n" + "Enter a row number: ");
+    //Display menu method
+    public static void displayMenuText() {
+        System.out.println("\n" + "1. Show the seats" + "\n" + "2. Buy a ticket" + "\n" + "3. Statistics" + "\n"
+                + "0. Exit");
         System.out.print("> ");
-        int rowNumber = scanner.nextInt();
-        int rowNumberBooked = rowNumber - 1;
+    }
 
+    //Buy a ticket method
+    public static void buyTicket(int numberOfRows,
+                                 int numberOfSeatsInRow,
+                                 int rowNumber,
+                                 int seatNumber,
+                                 ArrayList<Integer> seatNumberBookedContainer,
+                                 ArrayList<Integer> rowNumberBookedContainer) {
 
-        System.out.println("Enter a seat number in that row: ");
-        System.out.print("> ");
-        int seatNumber = scanner.nextInt();
         int seatNumberBooked = seatNumber - 1;
+        int rowNumberBooked = rowNumber - 1;
+        rowNumberBookedContainer.add(rowNumberBooked);
+        seatNumberBookedContainer.add(seatNumberBooked);
+
         int totalNumberOfSeats = numberOfRows * numberOfSeatsInRow;
 
         if (totalNumberOfSeats < 60) {
-            calculateTicketPriceForLessThan60Seats();
+            System.out.println("\nTicket price: $" + calculateTicketPriceForLessThan60Seats());
         } else {
-            System.out.println("Ticket price: $" + calculateTicketPriceForMoreThan60Seats(numberOfRows, rowNumber));
+            System.out.println("\nTicket price: $" + calculateTicketPriceForMoreThanEqualTo60Seats(numberOfRows, rowNumber));
         }
-
-        displayMenuText(menuItem,
-                counter,
-                numberOfRows,
-                numberOfSeatsInRow,
-                cinemaRoom,
-                rowNumber,
-                seatNumber,
-                checkSum,
-                rowNumberBooked,
-                seatNumberBooked);
     }
 
-//    public static void storeSeatsBooked(int seatNumberBooked, int rowNumberBooked, int checkSum) {
-//
-//    }
-
-    //A method to show the seats 24/01/2021*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*
+    //Show the seats method
     public static void showTheSeats(int numberOfRows,
                                     int numberOfSeatsInRow,
                                     char[][] cinemaRoom,
-                                    int rowNumber,
-                                    int seatNumber,
-                                    int menuItem,
-                                    int checkSum,
-                                    int rowNumberBooked,
-                                    int seatNumberBooked) {
-
-
+                                    ArrayList<Integer> seatNumberBookedContainer,
+                                    ArrayList<Integer> rowNumberBookedContainer) {
         int counter = 0;
         printOutNumbersOfSeatsAtTop(numberOfSeatsInRow);
 
-        if (checkSum > 0) {
             for (int i = 0; i < numberOfRows; i++) {
                 System.out.print(++counter + " ");
                 for (int j = 0; j < numberOfSeatsInRow; j++) {
-                    if (i == rowNumberBooked && j == seatNumberBooked) {
-                        System.out.print(cinemaRoom[i][j] = 'B');
-                    } else {
-                        System.out.print(cinemaRoom[i][j] = 'S');
+                    for (int k = 0; k < rowNumberBookedContainer.size(); k++) {
+                        if (i == rowNumberBookedContainer.get(k) && j == seatNumberBookedContainer.get(k)) {
+                            System.out.print(cinemaRoom[i][j] = 'B');
+                            System.out.print(" ");
+                        }
                     }
-                    System.out.print(" ");
-                }
-                System.out.println();
-            }
-        } else {
-            for (int i = 0; i < numberOfRows; i++) {
-                System.out.print(++counter + " ");
-                for (int j = 0; j < numberOfSeatsInRow; j++) {
-
+                    if (cinemaRoom[i][j] == 'B') continue;
                     System.out.print(cinemaRoom[i][j] = 'S');
                     System.out.print(" ");
                 }
                 System.out.println();
             }
-        }
-        displayMenuText(menuItem,
-                counter,
-                numberOfRows,
-                numberOfSeatsInRow,
-                cinemaRoom,
-                rowNumber,
-                seatNumber,
-                checkSum,
-                rowNumberBooked,
-                seatNumberBooked);
     }
 
+    //Statistics method
+    public static void displayStatistics(int numberOfTicketsPurchased,
+                                         int currentIncome,
+                                         int numberOfRows,
+                                         int numberOfSeatsInRow) {
+
+        System.out.println("\nNumber of purchased tickets: " + numberOfTicketsPurchased);
+
+        int totalNumberOfSeats = numberOfRows * numberOfSeatsInRow;
+        double ticketsPurchasedPercentage = (double) numberOfTicketsPurchased / totalNumberOfSeats * 100;
+        System.out.printf("Percentage: %.2f", ticketsPurchasedPercentage);
+        System.out.println("%");
+
+        System.out.printf("Current income: $%s", currentIncome);
+        System.out.println();
+
+        System.out.printf("Total income: $%s", calculateTotalIncome(numberOfRows, numberOfSeatsInRow));
+        System.out.print("");
+
+        System.out.println();
+    }
 
     // A method to print out the number of seats at the very top
     public static void printOutNumbersOfSeatsAtTop(int numberOfSeatsInRow) {
@@ -146,19 +105,18 @@ public class Tickets {
         for (int i = 0; i < numberOfSeatsInRow + 1; i++) {
             if (i == 0) {
                 System.out.print("  ");
-            }
-            else {
+            } else {
                 System.out.print(i + " ");
             }
         }
         System.out.println();
     }
 
-    public static void calculateTicketPriceForLessThan60Seats() {
-        System.out.println("Ticket price: $10");
+    public static int calculateTicketPriceForLessThan60Seats() {
+        return 10;
     }
 
-    public static int calculateTicketPriceForMoreThan60Seats(int numberOfRows, int rowNumber) {
+    public static int calculateTicketPriceForMoreThanEqualTo60Seats(int numberOfRows, int rowNumber) {
 
         int evenNumberOfRows = numberOfRows % 2;
         int firstHalfSeats;
@@ -166,72 +124,147 @@ public class Tickets {
         int ticketPrice = 0;
 
         switch (evenNumberOfRows) {
-            case 0 -> {
+            case 0:
                 firstHalfSeats = numberOfRows / 2;
                 if (rowNumber <= firstHalfSeats) {
                     ticketPrice = 10;
                 } else
                     ticketPrice = 8;
-            }
-            case 1 -> {
+                break;
+            case 1:
                 noOfFrontRows = (((numberOfRows + 1) / 2) - 1);
                 if (rowNumber <= noOfFrontRows) {
                     ticketPrice = 10;
                 } else {
                     ticketPrice = 8;
                 }
-            }
+                break;
         }
         return ticketPrice;
     }
-/*--MAIN----MAIN----MAIN----MAIN----MAIN----MAIN----MAIN----MAIN----MAIN----MAIN----MAIN----MAIN----MAIN----MAIN--*/
+
+    public static int calculateTotalIncome(int numberOfRows, int numberOfSeatsInRow) {
+        int evenNumberOfRows = numberOfRows % 2;
+        int incomeFromFrontRows;
+        int incomeFromBackRows;
+        int totalIncome = 0;
+        if (numberOfRows * numberOfSeatsInRow < 60) {
+            totalIncome = numberOfRows * numberOfSeatsInRow * 10;
+        } else {
+            switch (evenNumberOfRows) {
+                case 0:
+                    totalIncome = (numberOfRows / 2 * numberOfSeatsInRow * 10) + (numberOfRows / 2 * numberOfSeatsInRow * 8);
+                    break;
+                case 1:
+                    incomeFromFrontRows = (((numberOfRows + 1) / 2) - 1) * numberOfSeatsInRow * 10;
+                    incomeFromBackRows = (numberOfRows + 1) / 2 * numberOfSeatsInRow * 8;
+                    totalIncome = incomeFromFrontRows + incomeFromBackRows;
+                    break;
+            }
+        }
+        return totalIncome;
+    }
+
+    public static int getRowNumber(Scanner scanner) {
+        int rowNumber;
+        System.out.println("\nEnter a row number: ");
+        System.out.print("> ");
+        rowNumber = scanner.nextInt();
+        return rowNumber;
+    }
+    public static int getSeatNumber(Scanner scanner) {
+        int seatNumber;
+        System.out.println("Enter a seat number in that row: ");
+        System.out.print("> ");
+        seatNumber = scanner.nextInt();
+        return seatNumber;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int numberOfSeatsInRow;
-        int numberOfRows;
-        var counter = 0;
         var rowNumber = 0;
         var seatNumber = 0;
-        var menuItem = 0;
-        var checkSum = 0;
-        var rowNumberBooked = 0;
-        var seatNumberBooked = 0;
+        int menuItem;
+        int numberOfTicketsPurchased = 0;
+        int currentIncome = 0;
 
+        ArrayList<Integer> seatNumberBookedContainer = new ArrayList<>();
+        ArrayList<Integer> rowNumberBookedContainer = new ArrayList<>();
 
         System.out.println("Enter the number of rows:");
         System.out.print("> ");
-        numberOfRows = scanner.nextInt();
-
-        while (numberOfRows > 9) {
-            numberOfRows = scanner.nextInt();
-        }
+        int numberOfRows = getNumberOfRows();
 
         System.out.println("Enter the number of seats in each row:");
         System.out.print("> ");
-        numberOfSeatsInRow = scanner.nextInt();
-        char[][] cinemaRoom = new char[numberOfRows][numberOfSeatsInRow];
+        int numberOfSeatsInRow = getNumberOfSeatsInRow();
 
-        displayMenuText(menuItem,
-                counter,
-                numberOfRows,
-                numberOfSeatsInRow,
-                cinemaRoom,
-                rowNumber,
-                seatNumber,
-                checkSum,
-                rowNumberBooked,
-                seatNumberBooked);
+        char[][] cinemaRoom = new char[numberOfRows][numberOfSeatsInRow];
+        boolean isOutOfBounds = !(rowNumber > numberOfRows || seatNumber > numberOfSeatsInRow);
+
+        displayMenuText();
         menuItem = scanner.nextInt();
-        selectMenuItem(menuItem,
-                counter,
-                numberOfRows,
-                numberOfSeatsInRow,
-                cinemaRoom,
-                rowNumber,
-                seatNumber,
-                checkSum,
-                rowNumberBooked,
-                seatNumberBooked);
+
+        while (menuItem != 0) {
+            switch (menuItem) {
+                case 1:
+                    showTheSeats(numberOfRows,
+                            numberOfSeatsInRow,
+                            cinemaRoom,
+                            seatNumberBookedContainer,
+                            rowNumberBookedContainer);
+                    displayMenuText();
+                    break;
+                case 2:
+
+                    numberOfTicketsPurchased++;
+                    rowNumber = getRowNumber(scanner);
+                    seatNumber = getSeatNumber(scanner);
+                    while (isOutOfBounds || rowNumberBookedContainer.contains(rowNumber - 1)) {
+                        if (isOutOfBounds) {
+                            System.out.println("Wrong input!");
+                            rowNumber = getRowNumber(scanner);
+                            seatNumber = getSeatNumber(scanner);
+                            break;
+                        } else if (seatNumberBookedContainer.contains(seatNumber - 1)) {
+                            for (int i = 0; i < rowNumberBookedContainer.size() && i < seatNumberBookedContainer.size(); i++) {
+                                    if (rowNumberBookedContainer.get(i).equals(rowNumber - 1) && seatNumberBookedContainer.get(i).equals(seatNumber - 1)) {
+                                        System.out.println("\nThat ticket has already been purchased!");
+                                        rowNumber = getRowNumber(scanner);
+                                        seatNumber = getSeatNumber(scanner);
+                                        break;
+                                    }
+                            }
+                        }
+                        break;
+                    }
+
+                    if (numberOfRows * numberOfSeatsInRow < 60 && numberOfTicketsPurchased > 0) {
+                        currentIncome += calculateTicketPriceForLessThan60Seats();
+                    } else if (numberOfRows * numberOfSeatsInRow >= 60 && numberOfTicketsPurchased > 0) {
+                        currentIncome += calculateTicketPriceForMoreThanEqualTo60Seats(numberOfRows, rowNumber);
+                    }
+                        buyTicket(numberOfRows,
+                                numberOfSeatsInRow,
+                                rowNumber,
+                                seatNumber,
+                                seatNumberBookedContainer,
+                                rowNumberBookedContainer);
+                        displayMenuText();
+
+                    break;
+                case 3:
+                    displayStatistics(numberOfTicketsPurchased,
+                            currentIncome,
+                            numberOfRows,
+                            numberOfSeatsInRow);
+                    displayMenuText();
+                    break;
+                case 0:
+                    return;
+            }
+            menuItem = scanner.nextInt();
+        }
     }
 }
